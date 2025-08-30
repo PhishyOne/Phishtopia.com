66936b3a46b05c4376e77565d89aea7fe9b06336
Chris Salmon <phishy0914@hotmail.com>
2025-08-30 05:10:25 +0000

    Removed Birthday

--- js/script.js
+++ js/script.js
@@ -1,9 +1,1 @@
 
-
-$("button.bday-button").click(function () {
-    $(this).fadeOut(500, function () {
-        $("img.bday-video").removeAttr("hidden");
-        $("img.bday-video").fadeIn(500);
-    });
-});
-
