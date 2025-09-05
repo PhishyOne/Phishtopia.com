5df7ceec2b3953fa5a22d0d63a6c59ede70092b7
Chris Salmon <phishy0914@hotmail.com>
2025-09-05 23:32:09 +0000

    Ash Birthday

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-
+document.getElementById('play-video').addEventListener('click', function () {
+    var video = document.getElementById('ash-birthday');
+    video.style.display = 'block'; /* Show the video */
+    video.play(); /* Play the video */
+    video.muted = false; /* Enable audio */
+    video.loop = true; /* Enable looping */
+    this.style.display = 'none'; /* Hide the button */
+});
