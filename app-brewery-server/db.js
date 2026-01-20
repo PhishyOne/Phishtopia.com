16dcfcea1d028efea9b7228e36a8c97a2862f300
Chris Salmon <phishy0914@hotmail.com>
2026-01-20 21:52:16 +0000

    Complete 33-3

--- app-brewery-server/db.js
+++ app-brewery-server/db.js
@@ -23,4 +23,3 @@ try {
 }
 
 export default db;
-
--- app-brewery-server/routes/project33-3.js
+++ app-brewery-server/routes/project33-3.js
@@ -3,26 +3,80 @@ import db from "../db.js";
 
 const router = express.Router();
 
-let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
-];
-
-router.get("/", (req, res) => {
-  res.render("project33-3", {
-    listTitle: "Today",
-    listItems: items,
+router.get("/", async(req, res) => {
+  try {
+    const result = await db.query(
+      "SELECT * FROM fullstack.items_33_3 ORDER BY id"
+    );
+    const items = result.rows;
+    res.render("project33-3", {
+      listTitle: "Today",
+      listItems: items,
   });
+  } catch (err) {
+    console.error("DB error:", err);
+    res.status(500).send("Database error");
+  }
 });
 
-router.post("/add", (req, res) => {
-  const item = req.body.newItem;
-  items.push({ title: item });
-  res.redirect("/project33-3");
+router.post("/add", async (req, res) => {
+  try {
+    const item = req.body.newItem;
+
+    if (!item || !item.trim()) {
+      return res.redirect("/project33-3");
+    }
+
+    await db.query(
+      "INSERT INTO fullstack.items_33_3 (title) VALUES ($1)",
+      [item.trim()]
+    );
+
+    res.redirect("/project33-3");
+  } catch (err) {
+    console.error("DB insert error:", err);
+    res.status(500).send("Failed to add item");
+  }
 });
 
-router.post("/edit", (req, res) => {});
+router.post("/edit", async (req, res) => {
+  try {
+    const item = req.body.updatedItemTitle;
+    const itemId = req.body.updatedItemId;
+    if (!item || !item.trim()) {
+      return res.redirect("/project33-3");
+    }
 
-router.post("/delete", (req, res) => {});
+    await db.query(
+      "UPDATE fullstack.items_33_3 SET title = $1 WHERE id = $2",
+      [item.trim(), itemId]
+    );
+
+    res.redirect("/project33-3");
+  } catch (err) {
+    console.error("DB insert error:", err);
+    res.status(500).send("Failed to edit item");
+  }
+});
+
+router.post("/delete", async (req, res) => {
+  try {
+    const itemId = req.body.deleteItemId;
+
+    if (!itemId) {
+      return res.redirect("/project33-3");
+    }
+
+    await db.query(
+      "DELETE FROM fullstack.items_33_3 WHERE id = $1",
+      [itemId]
+    );
+
+    res.redirect("/project33-3");
+  } catch (err) {
+    console.error("DB insert error:", err);
+    res.status(500).send("Failed to edit item");
+  }
+});
 
 export default router;
