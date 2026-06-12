d098d85552fd626b55c89333993c2953be23b817
Chris Salmon <phishy0914@hotmail.com>
2026-06-12 19:55:25 +0000

    Add shared path configuration

---
+++ src/config/paths.js
@@ -0,0 +1,11 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+export const srcDir = dirname(fileURLToPath(import.meta.url));
+export const rootDir = dirname(dirname(srcDir));
+export const viewsDir = join(rootDir, "views");
+export const publicDir = join(rootDir, "public");
+export const logsDir = join(rootDir, "logs");
+export const appBreweryPublicDir = join(rootDir, "app-brewery-server", "public");
+export const appBreweryStaticViewsDir = join(rootDir, "views", "app-brewery-static");
+export const projectAssetsDir = join(rootDir, "public", "projects");
