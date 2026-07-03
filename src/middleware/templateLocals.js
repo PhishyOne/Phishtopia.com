b74a85240f7d40630261dd3774712e5186cb6492
Chris Salmon <phishy0914@hotmail.com>
2026-07-03 21:21:58 +0000

    Use dynamic canonical URLs

--- src/middleware/templateLocals.js
+++ src/middleware/templateLocals.js
@@ -1,8 +1,18 @@
+function buildCanonicalUrl(req) {
+    const siteUrl = process.env.SITE_URL || "https://phishtopia.com";
+    const cleanBaseUrl = siteUrl.replace(/\/$/, "");
+
+    const pathOnly = req.path === "/" ? "/" : req.path.replace(/\/$/, "");
+
+    return `${cleanBaseUrl}${pathOnly}`;
+}
+
 export function templateLocals(req, res, next) {
     res.locals.extraStyles = [];
     res.locals.extraScripts = [];
     res.locals.user = req.session?.user || null;
     res.locals.currentUrl = req.originalUrl;
+    res.locals.canonicalUrl = buildCanonicalUrl(req);
 
     if (process.env.LOG_SESSIONS === "true") {
         console.log("SESSION:", {
