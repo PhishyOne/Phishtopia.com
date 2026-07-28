395d876fa106a30bc0db7a3e6c0f2f6ca3da6c52
Chris Salmon <phishy0914@hotmail.com>
2026-07-28 21:25:50 +0000

    Fix retired path blocker syntax

--- src/middleware/staticAssets.js
+++ src/middleware/staticAssets.js
@@ -1,12 +1,7 @@
 import express from "express";
 import { publicDir } from "../config/paths.js";
 
-const RETIRED_PUBLIC_PATH = /^\/(?:
-    static(?:\/|$)
-    |projects(?:\/|$)
-    |project(?:25|28|29|30|33-1|33-2|33-3|34)(?:\/|$)
-    |player-?int(?:\/|$)
-)/ix;
+const RETIRED_PUBLIC_PATH = /^\/(?:static(?:\/|$)|projects(?:\/|$)|project(?:25|28|29|30|33-1|33-2|33-3|34)(?:\/|$)|player-?int(?:\/|$))/i;
 
 export function registerStaticAssets(app) {
     app.use((req, res, next) => {
